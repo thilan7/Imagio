bab515752fe211fdb9a394c3d04464273e94fa78
thilan7 <thilan.14@cse.mrt.ac.lk>
2018-10-16 12:08:28 +0530

    Add files via upload

---
+++ src/imagio/EdgeDetection.java
@@ -0,0 +1,96 @@
+package imagio;
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+
+/**
+ *
+ * @author Thilan K Bandara
+ */
+public class EdgeDetection {
+    public Image prewwitt(Image image, String state,int tr){
+        //BufferedImage bufferedImage = new BufferedImage(image.getBufferedImage().getWidth(), image.getBufferedImage().getHeight(), BufferedImage.TYPE_INT_RGB);
+        Image returnImage;
+        if(state.equals("h")){ 
+            int[][] ed = new int[][]{{-1,-1,-1},{0,0,0},{1,1,1}};
+            returnImage=convolute(image, ed, tr);
+        }
+        else{
+            int[][] ed = new int[][]{{1,0,1},{-1,0,1},{-1,0,1}};
+            returnImage = convolute(image, ed, tr);
+        }
+        
+        return returnImage;
+    }
+    
+    public Image sobel(Image image, String state,int tr){
+        Image returnImage;
+        if(state.equals("h")){ 
+            int[][] ed = new int[][]{{-1,-2,-1},{0,0,0},{1,2,1}};
+            returnImage=convolute(image, ed, tr);
+        }
+        else{
+            int[][] ed = new int[][]{{1,0,1},{-2,0,2},{-1,0,1}};
+            returnImage = convolute(image, ed, tr);
+        }
+        
+        return returnImage;
+    }
+    
+    public Image laplasian(Image image, String state,int tr){
+        Image returnImage;
+        if(state.equals("h")){ 
+            int[][] ed = new int[][]{{0,1,0},{1,-4,1},{0,1,0}};
+            returnImage=convolute(image, ed, tr);
+        }
+        else{
+            int[][] ed = new int[][]{{0,-1,0},{-1,4,-1},{0,-1,0}};
+            returnImage = convolute(image, ed, tr);
+        }
+        
+        return returnImage;
+    }
+    
+    public int marixMul(int[][] a,int[][] b, int len){
+        int ans =0;
+        int val=0;
+        for(int i=0;i<len;i++){
+            for(int j=0;j<len;j++){
+                val=val+a[i][j]*b[i][j];
+            }
+        }
+        return val;
+    }
+    
+    public Image convolute(Image image,int[][] ed,int tr){
+        BufferedImage bufferedImage = new BufferedImage(image.getBufferedImage().getWidth(), image.getBufferedImage().getHeight(), BufferedImage.TYPE_INT_RGB);
+        for(int i=1;i<image.getBufferedImage().getWidth()-1;i++){
+            for(int j=1;j<image.getBufferedImage().getHeight()-1;j++){
+                Color color=null;
+                int[][] colors = new int[3][3];
+                for(int m=i-1;m<i+2;m++){
+                    for(int n=j-1;n<j+2;n++){
+                        color = new Color(image.getBufferedImage().getRGB(m, n));
+                        //System.out.println((m-i+1)+" "+(n-i+2));
+                        colors[m-i+1][n-j+1] = (color.getRed()+color.getGreen()+color.getBlue())/3;
+                        
+                        //System.out.println((color.getRed()+color.getGreen()+color.getBlue())/3 );
+                    }
+                }
+                if(Math.abs(marixMul(colors, ed, 3))>tr){
+                    //System.out.println(Math.abs(marixMul(colors, ed, 3)));
+                    bufferedImage.setRGB(i, j, new Color(color.getRed(),color.getGreen(),color.getBlue()).getRGB());
+                }
+                else{
+                    //System.out.println(Math.abs(marixMul(colors, ed, 3)));
+                    bufferedImage.setRGB(i, j, new Color(255,255,255).getRGB());
+                }
+                    
+            }
+        }
+        
+        Image returnImage = new Image(bufferedImage);
+        return returnImage;
+    }
+    
+}
---
+++ src/imagio/ImageHandler.java
@@ -0,0 +1,74 @@
+package imagio;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.imageio.ImageIO;
+
+/**
+ *
+ * @author Thilan K Bandara
+ */
+
+public class ImageHandler {
+    
+    private static ImageHandler imageHandler = null;
+    private ArrayList<Image> images;
+    
+    private ImageHandler(){
+        //imageHandler = new ImageHandler();
+    }
+    
+    public static ImageHandler getInstance(){
+        if(imageHandler==null){
+            imageHandler = new ImageHandler();
+        }
+        return imageHandler;
+    }
+    
+    public void setImage(File file) throws IOException{
+        images = new ArrayList<Image>();
+        images.add(new Image(ImageIO.read(file)));
+    }
+    
+    public void evolveImage(Image image){
+        image.setId(images.size());
+        //System.out.println(image.getId());
+        images.add(image);
+    }
+    
+    public Image getImage(){
+         return images.get(images.size()-1);
+    }
+    
+    public Image undo(int current){
+        if(current==0){
+            return null;
+        }
+        return images.get(current-1);
+    }
+    
+    public Image redo(int current){
+        if(current>=images.size()-1){
+            return null;
+        }
+        return images.get(current+1);
+    }
+    
+    public Image getOriginal(){
+        return images.get(0);
+    }
+    
+    public boolean saveImage(File file){
+        System.out.println(file.toString());
+        try{
+            ImageIO.write(getImage().getBufferedImage(), "png", file);
+            return true;
+        }
+        catch(IOException e){
+            return false;
+        }
+            
+    }
+    
+}
